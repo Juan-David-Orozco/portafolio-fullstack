32eca8d861b8d62fd2a5cbd0cab1c2244c7d401a
Juan-David-Orozco <ing.ele2018@gmail.com>
2023-09-28 19:58:46 -0500

    update diagramation cards

--- src/pages/FrontPage.jsx
+++ src/pages/FrontPage.jsx
@@ -24,14 +24,15 @@ export function FrontPage() {
   return (
     <>
       <main>
-        <div className="row align-items-center justify-content-center no-gutters mx-auto px-1">
-          <div className="col-12 mx-auto text-center my-1">
+        <div className="row align-items-center justify-content-center no-gutters mx-auto">
+          <div className="col-12 mx-auto">
             <div className="card">
-              <div className="card-header d-flex justify-content-between">
+              <div className="card-header d-flex justify-content-between bg-dark text-white">
                 <h4 className="card-title my-auto">Portafolio FrontEnd</h4>
-                <Link to={"/portafolio-fullstack"} className="btn btn-dark my-auto" >Volver a Inicio</Link>
+                <Link to={"/portafolio-fullstack"} className="btn btn-light btn-sm my-auto" >Volver a Inicio</Link>
               </div>
               <div className="card-body">
+                <h5>Bienvenido!!!</h5>
                 <p className="card-text">
                   Realiza un recorrido para revisar cada uno de los proyectos los cuales
                   emplean tecnologias modernas o frameworks para el desarrollo de interfaces
@@ -41,35 +42,31 @@ export function FrontPage() {
             </div>
           </div>
         </div>
-        <div className="row mx-auto align-items-center justify-content-center no-gutters px-1">
-          <div className="col-12 mx-auto text-center bg-light my-1 mx-auto p-4">
-            <div className="row mx-auto align-items-center justify-content-center no-gutters">
+        <div className="row mx-auto align-items-center no-gutters">
+          <div className="col-12 mx-auto bg-light p-5">
+            <div className="row mx-auto align-items-center no-gutters">
               {
                 projects.map((project) => (
-                  <div className="col-12 col-sm-10 col-md-6 p-2 mx-auto" key={project.id}>
+                  <div className="col-12 col-sm-10 col-md-6 col-lg-4 p-2 mx-auto" key={project.id}>
                     <div className="card border border-dark">
-                      <div className="card-header bg-dark text-white d-flex justify-content-between">
+                      <div className="card-header bg-secondary d-flex justify-content-between">
                         <h6 className="mb-0" >{project.title}</h6>
                         <img src={project.frame} className="icon" />
                       </div>
-                      <div className="card-body d-flex justify-content-between text-justify">
-                        <p>{project.content}</p>
-                      </div>
-                      <div className="card-footer d-flex justify-content-around">
-                        <div className="d-flex justify-content-between">
-                          <span className="text-dark d-none d-sm-inline">Visitar</span>
+                      <div className="card-body">
+                        <p className="pb-5" >{project.content}</p>
+                        <p>
                           <Link to={project.web} target="_blank" className="btn btn-outline-primary btn-sm mr-1">
-                            <span className="mr-2 mt-2" >Sitio web</span>
                             <img src="https://res.cloudinary.com/cloud-juan/image/upload/v1695936013/portfolio/rczxshnaxdvakbrbs6hd.png" className="icon" />
                           </Link>
-                        </div>
-                        <div className="d-flex justify-content-between">
-                          <span className="text-dark d-none d-sm-inline">Revisar</span>
-                          <Link to={project.github} target="_blank" className="btn btn-outline-dark btn-sm">
-                            <span className="mr-2">Repositorio</span>
+                          <span>Sitio Web</span>
+                        </p>
+                        <p>
+                          <Link to={project.github} target="_blank" className="btn btn-outline-dark btn-sm mr-1">
                             <img src="https://res.cloudinary.com/cloud-juan/image/upload/v1695932058/portfolio/a3w1xhhdqdjm4shgb5gn.png" className="icon" />
                           </Link>
-                        </div>
+                          <span>Portafolio</span>
+                        </p>
                       </div>
                     </div>
                   </div>
