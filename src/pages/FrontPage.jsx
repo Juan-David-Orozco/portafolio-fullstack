1654981fa60a1f932b8a6b65d180c847048bf24e
Juan-David-Orozco <ing.ele2018@gmail.com>
2023-09-28 19:12:46 -0500

    diagramation cards FrontPage

--- src/pages/FrontPage.jsx
+++ src/pages/FrontPage.jsx
@@ -24,12 +24,14 @@ export function FrontPage() {
   return (
     <>
       <main>
-        <div className="row mx-auto align-items-center justify-content-center no-gutters">
-          <div className="col-11 col-sm-10 col-md-9 mx-auto text-center my-3">
+        <div className="row align-items-center justify-content-center no-gutters mx-auto px-1">
+          <div className="col-12 mx-auto text-center my-1">
             <div className="card">
+              <div className="card-header d-flex justify-content-between">
+                <h4 className="card-title my-auto">Portafolio FrontEnd</h4>
+                <Link to={"/portafolio-fullstack"} className="btn btn-dark my-auto" >Volver a Inicio</Link>
+              </div>
               <div className="card-body">
-                <h4 className="card-title">Bienvenido!</h4>
-                <h6 className="card-subtitle mb-2 text-muted">Portafolio FrontEnd</h6>
                 <p className="card-text">
                   Realiza un recorrido para revisar cada uno de los proyectos los cuales
                   emplean tecnologias modernas o frameworks para el desarrollo de interfaces
@@ -39,44 +41,42 @@ export function FrontPage() {
             </div>
           </div>
         </div>
-        <div className="row mx-auto align-items-center justify-content-center no-gutters">
-          {
-            projects.map((project) => (
-              <div className="col-11 col-sm-8 col-md-6 col-lg-4 p-2 mx-auto" key={project.id}>
-                <div className="card">
-                  <div className="card-header bg-dark text-white d-flex justify-content-between">
-                    <h5 className="mb-0" >{project.title}</h5>
-                    <img src={project.frame} className="icon" />
+        <div className="row mx-auto align-items-center justify-content-center no-gutters px-1">
+          <div className="col-12 mx-auto text-center bg-light my-1 mx-auto p-4">
+            <div className="row mx-auto align-items-center justify-content-center no-gutters">
+              {
+                projects.map((project) => (
+                  <div className="col-12 col-sm-10 col-md-6 p-2 mx-auto" key={project.id}>
+                    <div className="card border border-dark">
+                      <div className="card-header bg-dark text-white d-flex justify-content-between">
+                        <h6 className="mb-0" >{project.title}</h6>
+                        <img src={project.frame} className="icon" />
+                      </div>
+                      <div className="card-body d-flex justify-content-between text-justify">
+                        <p>{project.content}</p>
+                      </div>
+                      <div className="card-footer d-flex justify-content-around">
+                        <div className="d-flex justify-content-between">
+                          <span className="text-dark d-none d-sm-inline">Visitar</span>
+                          <Link to={project.web} target="_blank" className="btn btn-outline-primary btn-sm mr-1">
+                            <span className="mr-2 mt-2" >Sitio web</span>
+                            <img src="https://res.cloudinary.com/cloud-juan/image/upload/v1695936013/portfolio/rczxshnaxdvakbrbs6hd.png" className="icon" />
+                          </Link>
+                        </div>
+                        <div className="d-flex justify-content-between">
+                          <span className="text-dark d-none d-sm-inline">Revisar</span>
+                          <Link to={project.github} target="_blank" className="btn btn-outline-dark btn-sm">
+                            <span className="mr-2">Repositorio</span>
+                            <img src="https://res.cloudinary.com/cloud-juan/image/upload/v1695932058/portfolio/a3w1xhhdqdjm4shgb5gn.png" className="icon" />
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div className="card-body">
-                    <p>{project.content}</p>
-                  </div>
-                  <div className="card-footer d-flex justify-content-around">
-                    <Link to={project.web} target="_blank" className="btn btn-primary">
-                      <span className="mr-2 d-none d-sm-inline" >Sitio web</span>
-                      <img src="https://res.cloudinary.com/cloud-juan/image/upload/v1695932411/portfolio/vm72n7diesv66v625sid.png" className="icon" />
-                    </Link>
-                    <Link to={project.github} target="_blank" className="btn btn-dark">
-                    <span className="mr-2 d-none d-sm-inline" >Repositorio</span>
-                      <img src="https://res.cloudinary.com/cloud-juan/image/upload/v1695932058/portfolio/a3w1xhhdqdjm4shgb5gn.png" className="icon" />
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            ))
-          }
-
-          {/* <div className="col-md-4 p-2">
-            <div className="card">
-              <div className="card-header d-flex justify-content-between align-items-center">
-                <h5>webservice-dragdrop-react</h5>
-                <Link to={"https://webservice-dragdrop-react.vercel.app/"} className="btn btn-sm" target="_blank">
-                  Visitar
-                </Link>
-              </div>
+                ))
+              }
             </div>
-          </div> */}
-
+          </div>
         </div>
       </main>
     </>
